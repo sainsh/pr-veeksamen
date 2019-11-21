a23523b4bdb549dc4fd56d085073f4ffa20ef3e5
sainsh <kfsainsh@gmail.com>
2019-11-21 09:53:46 +0100

    removed table functionality from indes.js created mixin for table in index.jade no more double table

--- routes/index.js
+++ routes/index.js
@@ -13,41 +13,12 @@ router.get('/', function(req, res, next) {
     database: "ikea-trådfri"
   })
 
-var out = `
-    table
-      tr
-      th ID
-      th Navn
-      th Tændt
-      th StrømForbrug
-      th AktuelForbrug
-      th Styrke
-      th Farve
-      th TypeNummer
-      th SoftwareVersion`;
-
     con.connect(function(err){
       if(err) throw err;
       con.query("SELECT * FROM smartpærer", function(err, result){
           if(err) throw err;
-
-        
-
-          for(var i = 0; i < result.length; i++){
-            out += `
-            tr
-              td ${result[i].ID}
-              td ${result[i].Navn}
-              td ${Boolean(result[i].Tændt)}
-              td ${result[i].Strømforbug}
-              td ${result[i].AktuelForbrug}
-              td ${result[i].Styrke}
-              td ${result[i].Farve}
-              td ${result[i].TypeNummer}
-              td ${result[i].SoftwareVersion}`;
-          }
-          console.log(out);
-          res.render('index', {output: out});
+          
+          res.render('index', {output: result});
       })
   })
 
