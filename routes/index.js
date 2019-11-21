dba3db5b848b1e5c6d18fd44ed6763822cdc50cd
sainsh <kfsainsh@gmail.com>
2019-11-21 09:19:46 +0100

    removed table.js and moved function to index.js added table to index.jade displays table on site but shows table 2 times

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,58 @@
 var express = require('express');
 var router = express.Router();
-const path = require('path');
+var mysql = require('mysql')
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var localpath = path.resolve(__dirname+'/../views/index2.html')
-  res.sendFile(localpath);
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "ikea-trådfri"
+  })
+
+var out = `
+    table
+      tr
+      th ID
+      th Navn
+      th Tændt
+      th StrømForbrug
+      th AktuelForbrug
+      th Styrke
+      th Farve
+      th TypeNummer
+      th SoftwareVersion`;
+
+    con.connect(function(err){
+      if(err) throw err;
+      con.query("SELECT * FROM smartpærer", function(err, result){
+          if(err) throw err;
+
+        
+
+          for(var i = 0; i < result.length; i++){
+            out += `
+            tr
+              td ${result[i].ID}
+              td ${result[i].Navn}
+              td ${Boolean(result[i].Tændt)}
+              td ${result[i].Strømforbug}
+              td ${result[i].AktuelForbrug}
+              td ${result[i].Styrke}
+              td ${result[i].Farve}
+              td ${result[i].TypeNummer}
+              td ${result[i].SoftwareVersion}`;
+          }
+          console.log(out);
+          res.render('index', {output: out});
+      })
+  })
+
+
+
 
 });
 
